43fb7ff39786e7c7e2f96ff07a340db9df737282
xiulian <1505359119@qq.com>
2018-06-26 22:53:21 +0800

    update Inception V3

--- Inception Net/InceptionNetV3.py
+++ Inception Net/InceptionNetV3.py
@@ -2,18 +2,29 @@ import tensorflow as tf
 import time,math
 from datetime import datetime
 
+#调用slim模块
 slim = tf.contrib.slim
+#定义一个产生截断正态分布的函数
 trunc_normal = lambda std:tf.truncated_normal_initializer(0.0,std)
+
+'''
+定义产生默认参数的函数
+'''
 def inception_v3_arg_scope(weight_decay=4e-4,std=0.1,batch_norm_var_collection="moving_vars"):
     batch_norm_params = {"decay":0.9997,"epsilon":0.001,"updates_collections":tf.GraphKeys.UPDATE_OPS,
                          "variables_collections":{"beta":None,"gamma":None,
                           "moving_mean":[batch_norm_var_collection],"moving_variance":[batch_norm_var_collection]}}
-    with slim.arg_scope([slim.conv2d],weights_initializer=tf.truncated_normal_initializer(stddev=std),
-                        activation_fn=tf.nn.relu,normalizer_fn=slim.batch_norm,
-                        normalizer_params=batch_norm_params) as sc:
-        return sc
+    with slim.arg_scope([slim.conv2d,slim.fully_connected],weights_regularizer=slim.l2_regularizer(weight_decay)):
+        with slim.arg_scope([slim.conv2d],weights_initializer=tf.truncated_normal_initializer(stddev=std),
+                            activation_fn=tf.nn.relu,normalizer_fn=slim.batch_norm,
+                            normalizer_params=batch_norm_params) as sc:
+            return sc
 
+'''
+定义Inception V3的网络结构
+'''
 def inception_v3_base(inputs,scope=None):
+    #保存关键节点
     end_points = {}
     with tf.variable_scope(scope,"InceptionV3",[inputs]):
         with slim.arg_scope([slim.conv2d,slim.max_pool2d,slim.avg_pool2d],stride=1,padding="VALID"):
@@ -24,6 +35,7 @@ def inception_v3_base(inputs,scope=None):
             net = slim.conv2d(net,80,[1,1],scope="Conv2d_3b_1x1")
             net = slim.conv2d(net,192,[3,3],scope="Conv2d_4a_3x3")
             net = slim.max_pool2d(net,[3,3],stride=2,scope="MaxPool5a_3x3")
+    #定义Inception module
     with slim.arg_scope([slim.conv2d,slim.max_pool2d,slim.avg_pool2d],stride=1,padding="SAME"):
         with tf.variable_scope("Mixed_5b"):
             with tf.variable_scope("Branch_0"):
@@ -67,6 +79,7 @@ def inception_v3_base(inputs,scope=None):
                 branch_3 = slim.avg_pool2d(net,[3,3],scope="AvgPool_0a_3x3")
                 branch_3 = slim.conv2d(branch_3,64,[1,1],scope="Conv2d_0b_1x1")
             net = tf.concat([branch_0,branch_1,branch_2,branch_3],3)
+
         with tf.variable_scope("Mixed_6a"):
             with tf.variable_scope("Branch_0"):
                 branch_0 = slim.conv2d(net,384,[3,3],stride=2,padding="VALID",scope="Conv2d_1a_1x1")
